90a6d07dee85952ec5469e482502f13906f161ec
Lianne Meah <aca10lfm@sheffield.ac.uk>
2013-05-05 18:48:59 +0100

    Added RSpec let function for base title in spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
@@ -12,8 +14,9 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+      #page.should have_selector('title',
+       # :text => "Ruby on Rails Tutorial Sample App | Home")
+    page.should have_selector('title', :text => "#{base_title} | Home")
   #describe "GET /static_pages" do
     #it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -30,8 +33,9 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Help")
+      #page.should have_selector('title',
+      #  :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -43,8 +47,9 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | About Us")
+      #page.should have_selector('title',
+       # :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
@@ -56,9 +61,9 @@ describe "StaticPages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      #page.should have_selector('title', :text => "#{base_title} | Contact")
-      page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+      #page.should have_selector('title',
+      #:text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
